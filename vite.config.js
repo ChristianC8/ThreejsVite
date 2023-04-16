d9dab007978afee3de01b82b0ea76e5ef3fa0424
ChristianC <110044310+ChristianC8@users.noreply.github.com>
2023-04-16 19:34:50 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,10 @@ import { defineConfig } from "vite"
 
 export default defineConfig({
   base: '/ThreejsVite/',
+  assets: [
+    './src/assets',
+    './FBXS' // ruta de tu carpeta FBX
+  ]
   
 }) 
 
