fd7a2a9f241315b4354f4caf35285ca8ca763b17
ChristianC <110044310+ChristianC8@users.noreply.github.com>
2023-04-16 18:33:41 -0300

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,14 +1,7 @@
 import { defineConfig } from "vite"
 import vue from '@vitejs/plugin-vue'
 
-module.exports = {
-    publicPath: 'ThreejsVite'
-  }
 
-/* export default defineConfig({
-  base: '/ThreejsVite/',
-  plugins: [vue()]
-}) */
 export default {
   base: "/ThreejsVite/",
 }
