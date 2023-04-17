d666acecb0023d4e8b4f6df1036d26c81387a2d4
ChristianC <110044310+ChristianC8@users.noreply.github.com>
2023-04-17 14:20:48 -0300

    nj

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,9 @@
 import { defineConfig } from "vite";
-import { copy } from 'vite-plugin-copy';
 
 
 export default defineConfig({
   base: '/ThreejsVite/',
-  plugins: [
-    copy({
-      targets: [
-        { src: 'FBXS/ramen.glb', dest: 'dist/FBXS' },
-      ],
-      hook: 'buildStart',
-    }),
-  ],
+
 
 
 
