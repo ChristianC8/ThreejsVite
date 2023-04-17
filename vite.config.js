96c667f359fa52350b8d955e1dfa0a8be89835ef
ChristianC <110044310+ChristianC8@users.noreply.github.com>
2023-04-17 14:51:08 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,14 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   base: '/ThreejsVite/',
-
+  build: {
+    assetsDir: 'assets',
+    rollupOptions: {
+      input: {
+        main: 'index.html'
+      }
+    }
+  }
 
 
 
